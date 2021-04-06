48c23028048129ca8cabc7e9ada5bd5f8148ac2f
kawamoto-yuta <kawamoto@mediowl.co.jp>
2021-04-06 18:22:16 +0900

    コミットし忘れていました。 こちら検索結果画面です。

---
+++ resources/views/todo/search.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @if (session('message'))
+                <div class="">
+                    <p>{{ session('message') }}</p>
+                </div>
+            @endif
+            <form action="{{ url('/todo/search') }}" method="get">
+                <div class="">
+                    <label>タイトル</label>
+                    <input type="text" name="title" value="{{ $title ?? '' }}">
+                </div>
+                <div class="" style="margin-top: 10px;">
+                    <label>詳細</label>
+                    <input type="text" name="content" value="{{ $content ?? '' }}">
+                </div>
+                <div>
+                    <label>ステータス</label>
+                    <input type="checkbox" name="incomplete" value=0>未完了
+                    <input type="checkbox" name="completed" value=1>完了
+                </div>
+                <input type="submit" value="検索" class="">
+            </form>
+
+
+            <div class="card">
+                <div class="card-header">検索結果</div>
+                @if (session('status'))
+                <div class="card-body">
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                </div>
+                @endif
+                <div class="row">
+                    @if(!empty($in_c_s_todos))
+                    @foreach($in_c_s_todos as $incomplete)
+                    <div class="col-sm-4">
+                        <a style="text-decoration:none; padding-bottom:5px;" href="{{ action('TodoController@show', ['id' => $incomplete->id]) }}'">
+                            <div class="incompletes card" style="margin-top:20px; padding-top:5px;">
+                                <div class="title" style="margin-left:15px; color:black;"><b>{{Str::limit($incomplete->title, 22, '…' )}}</b></div>
+                                @if(!is_null($incomplete->content))
+                                <div style="color:#bfbfbf; margin-left:15px;">{{Str::limit($incomplete->content, 24, '…' )}}</div>
+                                @else
+                                <div style="color:#00000000;">空白</div>
+                                @endif
+                                <div style="display:flex; justify-content:flex-end;">
+                                    <a style="text-decoration: none; margin-right:5px;" href="{{ action('TodoController@edit', ['id' => $incomplete->id]) }}'">✏️</a>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    @endforeach
+                    @endif
+                    @if(!empty($c_s_todos))
+                    @foreach($c_s_todos as $complete)
+                    <div class="col-sm-4">
+                        <a style="text-decoration:none; padding-bottom:5px;" href="{{ action('TodoController@show', ['id' => $complete->id]) }}'">
+                            <div class="completes card" style="margin-top:20px; padding-top:5px; background-color: #dfdfdf">
+                                <div class="title" style="margin-left:15px; color:black;"><b>✅ {{Str::limit($complete->title, 18, '…' )}}</b></div>
+                                @if(!is_null($complete->content))
+                                <div style="color:#bfbfbf; margin-left:15px;">{{Str::limit($complete->content, 24, '…' )}}</div>
+                                @else
+                                <div style="color:#00000000;">空白</div>
+                                @endif
+                                <div style="display:flex; justify-content:flex-end;">
+                                    <a style="text-decoration: none; margin-right:5px;" href="{{ action('TodoController@edit', ['id' => $complete->id]) }}'">✏️</a>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    @endforeach
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
