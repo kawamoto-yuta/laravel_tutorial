e4dc94ac9b33cb3fca8eb13421704693cd994ed1
kawamoto-yuta <kawamoto@mediowl.co.jp>
2021-04-06 14:53:24 +0900

    新規登録画面をbootstrapで編集。

--- resources/views/todo/add.blade.php
+++ resources/views/todo/add.blade.php
@@ -1,28 +1,38 @@
 @extends('layouts.app')
 @section('content')
-<p>新規登録</p>
 @if ($errors->any())
-<div style="color:red;">
-<ul>
-	@foreach ($errors->all() as $error)
-	<li>{{ $error }}</li>
-	@endforeach
-</ul>
+<div class="row justify-content-center" style="color:red;">
+	<div class="col-md-8">
+		<ul style="list-style: none;">
+			@foreach ($errors->all() as $error)
+			<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
 </div>
 @endif
-{{Form::open(['action' => 'TodoController@addPost', 'files' => true])}}
+<div class="row justify-content-center">
+	<div class="col-md-8">
+		<div class="card">
+			<div class="card-header">新規登録</div>
+			<div class="card-body">
+			{{Form::open(['action' => 'TodoController@addPost', 'files' => true])}}
 
-{{Form::label('title','タイトル')}}
-{{Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'タイトル'])}}
+			{{Form::label('title','タイトル')}}
+			{{Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'タイトル'])}}
 
-{{Form::label('content','詳細')}}
-{{Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => '詳細'])}}
+			{{Form::label('content','詳細')}}
+			{{Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => '詳細'])}}
 
-{{Form::text('status', 0, ['style' => 'display: none;'])}}
-{{Form::text('place', "add", ['style' => 'display: none;'])}}
-{{Form::text('user_id', Auth::user()->id, ['style' => 'display: none;'])}}
+			{{Form::text('status', 0, ['style' => 'display: none;'])}}
+			{{Form::text('place', "add", ['style' => 'display: none;'])}}
+			{{Form::text('user_id', Auth::user()->id, ['style' => 'display: none;'])}}
 
-{{Form::submit('保存', ['class'=>'btn btn-primary btn-block'])}}
+			{{Form::submit('保存', ['class'=>'btn btn-primary btn-block'])}}
 
-{{Form::close()}}
+			{{Form::close()}}
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
