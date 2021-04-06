6b180df0f8d35e11c5cbde6a89fcac30dc57de2d
kawamoto-yuta <kawamoto@mediowl.co.jp>
2021-04-06 16:45:24 +0900

    トップページにおいて、編集ボタンを削除し、todo全体をクリックすれば詳細画面へ遷移するように変更した。

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,34 +21,36 @@
                 <div class="row">
                     @foreach($incompletes as $incomplete)
                     <div class="col-sm-4">
-                        <div class="incompletes card" style="margin-top:20px; padding-top:5px;">
-                            <div class="title" style="margin-left:15px;"><b>{{Str::limit($incomplete->title, 22, '…' )}}</b></div>
-                            @if(!is_null($incomplete->content))
-                            <div style="color:#bfbfbf; margin-left:15px;">{{Str::limit($incomplete->content, 24, '…' )}}</div>
-                            @else
-                            <div style="color:#00000000;">空白</div>
-                            @endif
-                            <div style="display:flex; justify-content:flex-end;">
-                                <a style="text-decoration: none; margin-right:15px;" href="{{ action('TodoController@edit', ['id' => $incomplete->id]) }}'">✏️</a>
-                                <a style="text-decoration: none; margin-right:5px;" href="{{ action('TodoController@show', ['id' => $incomplete->id]) }}'">ⓘ</a>
+                        <a style="text-decoration:none; padding-bottom:5px;" href="{{ action('TodoController@show', ['id' => $incomplete->id]) }}'">
+                            <div class="incompletes card" style="margin-top:20px; padding-top:5px;">
+                                <div class="title" style="margin-left:15px; color:black;"><b>{{Str::limit($incomplete->title, 22, '…' )}}</b></div>
+                                @if(!is_null($incomplete->content))
+                                <div style="color:#bfbfbf; margin-left:15px;">{{Str::limit($incomplete->content, 24, '…' )}}</div>
+                                @else
+                                <div style="color:#00000000;">空白</div>
+                                @endif
+                                <div style="display:flex; justify-content:flex-end;">
+                                    <a style="text-decoration: none; margin-right:5px;" href="{{ action('TodoController@edit', ['id' => $incomplete->id]) }}'">✏️</a>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     @endforeach
                     @foreach($completes as $complete)
                     <div class="col-sm-4">
-                        <div class="completes card" style="margin-top:20px; padding-top:5px; background-color: #dfdfdf">
-                            <div class="title" style="margin-left:15px;"><b>✅ {{Str::limit($complete->title, 18, '…' )}}</b></div>
-                            @if(!is_null($complete->content))
-                            <div style="color:#bfbfbf; margin-left:15px;">{{Str::limit($complete->content, 24, '…' )}}</div>
-                            @else
-                            <div style="color:#00000000;">空白</div>
-                            @endif
-                            <div style="display:flex; justify-content:flex-end;">
-                                <a style="text-decoration: none; margin-right:15px;" href="{{ action('TodoController@edit', ['id' => $complete->id]) }}'">✏️</a>
-                                <a style="text-decoration: none; margin-right:5px;" href="{{ action('TodoController@show', ['id' => $complete->id]) }}'">ⓘ</a>
+                        <a style="text-decoration:none; padding-bottom:5px;" href="{{ action('TodoController@show', ['id' => $complete->id]) }}'">
+                            <div class="completes card" style="margin-top:20px; padding-top:5px; background-color: #dfdfdf">
+                                <div class="title" style="margin-left:15px; color:black;"><b>✅ {{Str::limit($complete->title, 18, '…' )}}</b></div>
+                                @if(!is_null($complete->content))
+                                <div style="color:#bfbfbf; margin-left:15px;">{{Str::limit($complete->content, 24, '…' )}}</div>
+                                @else
+                                <div style="color:#00000000;">空白</div>
+                                @endif
+                                <div style="display:flex; justify-content:flex-end;">
+                                    <a style="text-decoration: none; margin-right:5px;" href="{{ action('TodoController@edit', ['id' => $complete->id]) }}'">✏️</a>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     @endforeach
                 </div>
