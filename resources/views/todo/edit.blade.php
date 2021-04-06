cece8cf22cd19221dda4f795e2542d7624af8dd1
kawamoto-yuta <kawamoto@mediowl.co.jp>
2021-04-06 15:14:07 +0900

    todo新規登録画面、編集画面をbootstrapで編集した。

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -1,36 +1,54 @@
 @extends('layouts.app')
 @section('content')
-<p>編集</p>
 @if ($errors->any())
-<div style="color:red;">
-<ul>
-	@foreach ($errors->all() as $error)
-	<li>{{ $error }}</li>
-	@endforeach
-</ul>
+<div class="row justify-content-center" style="color:red;">
+	<div class="col-md-8">
+		<ul style="list-style: none;">
+			@foreach ($errors->all() as $error)
+			<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
 </div>
 @endif
-{{Form::open(['action' => 'TodoController@editPost', 'files' => true])}}
+<div class="row justify-content-center">
+	<div class="col-md-8">
+		<div class="card">
+			<div class="card-header">編集</div>
+			<div class="card-body">
+			{{Form::open(['action' => 'TodoController@editPost', 'files' => true])}}
+				{{Form::label('title','タイトル')}}
+				{{Form::text('title', $todo->title, ['class' => 'form-control'])}}
+				<div  style="margin-top: 15px;">
+					{{Form::label('content','詳細')}}
+					{{Form::textarea('content', $todo->content, ['class' => 'form-control'])}}
+				</div>
+				<div  style="margin-top: 15px;">
+					{{Form::radio('status', 0, (old('status')== 0 ? true: ($todo->status == 0))? true:false, ['class' => 'circle'])}}
+					<span class="radio-button__icon">未完了</span>
+					<span style="margin-left:20px;"></span>
+					{{Form::radio('status', 1, (old('status')== 1 ? true: ($todo->status == 1))? true:false, ['class' => 'circle'])}}
+					<span class="radio-button__icon">完了</span>
+				</div>
 
-{{Form::label('title','タイトル')}}
-{{Form::text('title', $todo->title, ['class' => 'form-control'])}}
+				{{Form::text('place', "edit/{$todo->id}", ['style' => 'display: none;'])}}
+				{{Form::text('id', "{$todo->id}", ['style' => 'display: none;'])}}
 
-{{Form::label('content','詳細')}}
-{{Form::textarea('content', $todo->content, ['class' => 'form-control'])}}
-
-{{Form::radio('status', 0, (old('status')== 0 ? true: ($todo->status == 0))? true:false, ['class' => 'circle'])}}
-<span class="radio-button__icon">未完了</span>
-{{Form::radio('status', 1, (old('status')== 1 ? true: ($todo->status == 1))? true:false, ['class' => 'circle'])}}
-<span class="radio-button__icon">完了</span>
-
-{{Form::text('place', "edit/{$todo->id}", ['style' => 'display: none;'])}}
-{{Form::text('id', "{$todo->id}", ['style' => 'display: none;'])}}
-
-{{Form::submit('保存', ['class'=>'btn btn-primary btn-block'])}}
-
-{{Form::close()}}
-
-{!! Form::model($todo, ['action' => ['TodoController@destory', $todo->id]]) !!}
-    {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
-{!! Form::close() !!}
+				<div style="margin-top: 20px;">
+					<div class="row">
+						<div class="col-md-2">
+							{{Form::submit('保存', ['class'=>'btn btn-primary btn-block'])}}
+						{{Form::close()}}
+						</div>
+						<div class="col-md-1">
+						{!! Form::model($todo, ['action' => ['TodoController@destory', $todo->id]]) !!}
+							{!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+						{!! Form::close() !!}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
